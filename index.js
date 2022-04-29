0713de71b52b6ce2f1ec042473911cd01c3bcc7f
eshakkhan29 <eshakkhan29@gmail.com>
2022-04-29 16:40:30 +0600

    added load data by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,12 +17,19 @@ async function run() {
     try {
         await client.connect();
         const bikeCollection = client.db("Bike-details").collection("details");
-
+        //  load all data
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = bikeCollection.find(query);
             const products = await cursor.limit(6).toArray();
             res.send(products);
+        })
+        //  load data with id
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const quarry = { _id: ObjectId(id) };
+            const product = await bikeCollection.findOne(quarry);
+            res.send(product);
         })
 
     } finally {
